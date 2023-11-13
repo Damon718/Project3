c533b250de42339f1f06c383f065c6324940aa0f
Damon718 <144197240+Damon718@users.noreply.github.com>
2023-11-13 23:28:52 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,45 @@
+function jump(){
+  window.location.href="https://www.iit.edu/";
+}
+
+function initMap() {
+    const myLatLng = {lat: 41.83155375445465, lng: -87.62724193181992};
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 15,
+      center: myLatLng,
+    });
+  
+    const contentString =
+      '<div id="content">' +
+      '<div id="siteNotice">' +
+      "</div>" +
+      '<h1 id="firstHeading" class="firstHeading">IIT</h1>' +
+      '<div id="bodyContent">' +
+      "<p><b>IIT</b>, also referred to as <b>Illinois Tech</b>, small private university" +
+      "in the south side of Chicago." +
+      " It was ranked number 23 in the Wall Street Journals list of college rankings." +
+      " It's largest department is the computer science department followed by the ITM department." +
+      "</div>" + "</div>";
+    
+    const infowindow = new google.maps.InfoWindow({
+      content: contentString,
+      ariaLabel: "IIT",
+    });
+    
+    const marker = new google.maps.Marker({
+      position: myLatLng,
+      map,
+      title: "IIT",
+    }); 
+  
+    marker.addListener("click", () => {
+      infowindow.open({
+        anchor: marker,
+        map,
+      });
+    });
+    const trafficLayer = new google.maps.TrafficLayer();
+    trafficLayer.setMap(map);
+   }
+  window.initMap = initMap;
+ 
